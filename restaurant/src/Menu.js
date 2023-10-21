8470115f773ddb98ddbad5d4877e502fac2c654f
Jialin Li <jli511@ucsc.edu>
2023-10-21 14:18:13 -0700

    fixed menu

--- restaurant/src/Menu.js
+++ restaurant/src/Menu.js
@@ -1,9 +1,18 @@
 import React from "react";
 import './Menu.css'
 import SharedContext from './utility/context'; 
-function Menu(){
-    const {menuData, setMenuData} = React.useContext(SharedContext);
-    console.log(menuData)
+
+function Menu() {
+    const { menuData, setMenuData } = React.useContext(SharedContext);
+
+    // Check if menuData exists and has categories
+    if (!menuData || !menuData[0] || !menuData[0].categories) {
+        return <div>Loading...</div>;
+    }
+
+    // Extract categories from menuData
+    const categories = menuData[0].categories;
+
     return (
         <div id='Menu'>
             <div className="menuItem">
@@ -11,10 +20,35 @@ function Menu(){
                 <div>One side one entree</div>
                 <div>Price</div>
                 <div className='menuItemPic'>pic</div> 
-                {menuData.categories}
             </div>
+
+            {/* Map through categories and their items */}
+            {Object.keys(categories).map((sectionName) => {
+                const section = categories[sectionName];
+                return (
+                    <div key={sectionName}>
+                        <h2>{section.description}</h2>
+                        {Object.keys(section.items).map((itemName) => {
+                            const item = section.items[itemName];
+                            return (
+                                <div key={itemName}>
+                                    <h3>{itemName}</h3>
+                                    <p>Description: {item.description}</p>
+                                    <p>Price: {item.price}</p>
+                                    <p>Options:</p>
+                                    <ul>
+                                        {item.options["Spice level"].map((option, index) => (
+                                            <li key={index}>{option.description}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
         </div>
-    )
+    );
 }
 
-export default Menu; 
+export default Menu;

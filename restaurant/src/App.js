936fdeb8adfeaef29fe839afabd2a09ea3ee7849
Jialin Li <jli511@ucsc.edu>
2023-10-21 00:49:14 -0700

    add menu list

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,13 +1,15 @@
 import Info from "./Info";
-
+import Menu from "./Menu"; 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
       </header>
       <Info />
+      <Menu /> 
     </div>
   );
 }
 
 export default App;
+ 
---
+++ restaurant/src/Menu.js
@@ -0,0 +1,16 @@
+import './Menu.css'
+
+function Menu(){
+    return (
+        <div id='Menu'>
+            <div className="menuItem">
+                <div>Combo meal special</div>
+                <div>One side one entree</div>
+                <div>Price</div>
+                <div className='menuItemPic'>pic</div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu; 

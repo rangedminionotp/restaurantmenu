6538ff331edd198c40b8d72fec980e6efb3902e3
Jialin Li <jli511@ucsc.edu>
2023-10-26 21:14:16 -0700

    add simple image upload

---
+++ restaurant/src/ImageUpload.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function ImageUpload() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleUpload = () => {
+    // Implement the image upload logic here
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageChange} />
+      <button onClick={handleUpload}>Upload Image</button>
+    </div>
+  );
+}
+
+export default ImageUpload;
